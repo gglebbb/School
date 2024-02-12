943211bc78e1e9f362adffd5562b5994279e6833
gglebbb <98395169+gglebbb@users.noreply.github.com>
2024-02-12 15:39:31 +0300

    Add files via upload

---
+++ zadanie3-2/Duo-dem.cs
@@ -0,0 +1,97 @@
+using System;
+
+namespace Gleb
+{
+    public sealed class DuoDem : BaseClass
+    {
+        public bool autofill = false;
+        private int[,] duo_arr;
+        private int[,] prom;
+
+        public DuoDem( bool autofill)
+        {
+            Console.WriteLine("DuoDemension");
+            CreateMaster();
+        }
+
+        public override void CreateMas()
+        {
+            Console.WriteLine("Creating array...");
+            Console.WriteLine("Do you want to turn on autofill?(True or False");
+            bool auto  = bool.Parse(Console.ReadLine());
+            if(auto)
+            {
+                Random l = new Random();
+                duo_arr = Random(l.Next(2,10), l.Next(2,10));
+            }
+            else
+            {
+                
+                Console.WriteLine("Length of your new array: ");
+                int length = int.Parse(Console.ReadLine());
+                Console.WriteLine("Wigth of your new array: ");
+                int wigth = int.Parse(Console.ReadLine());
+                duo_arr = Input(length, wigth);
+            }    
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine(duo_arr.Rank);
+            Console.WriteLine("Your array:");
+            for(int i = 0; i < duo_arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < duo_arr.GetLength(1); j++)
+                {
+                    Console.Write(duo_arr[i,j]+" ");
+                }
+                Console.Write("\n");
+            }
+        }
+        
+        public override void MidVal()
+        {
+            int allsum = 0;
+            int MidVal = 0;
+            for(int i = 0; i < duo_arr.GetLength(0); i++)
+            {
+                for(int j = 0; j < duo_arr.GetLength(1); j++)
+                {
+                    allsum = allsum + duo_arr[i,j];
+                }
+            }
+            MidVal = allsum/duo_arr.GetLength(0)/duo_arr.GetLength(1);
+            Console.WriteLine($"Mid value: {MidVal}");
+        }   
+    
+        private int[,] Input(int l, int w)
+        {
+            prom = new int[l, w];
+            for(int i = 0; i < l; i++)
+            {
+                Console.WriteLine($"{i+1}-ая строка:");
+                string[] str = Console.ReadLine().Split(' ');
+                for(int j = 0; j<str.Length; j++)
+                {
+                    prom[i,j] = int.Parse(str[j]);
+                }
+            }
+            return prom;
+        }
+
+        private int[,] Random(int l, int w)
+        {
+            prom = new int[l, w];
+            Random rand = new Random();
+            for (int i = 0; i < l; i++)
+            {
+                for(int j = 0; j<w; j++)
+                {
+                    prom[i,j] = rand.Next(-200, 200);
+                }
+            }
+            return prom;
+        }
+
+    }
+}
