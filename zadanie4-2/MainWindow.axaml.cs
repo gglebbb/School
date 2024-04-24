21af77dc6fc30ad75a1c6f03ba3f125043e154fa
gglebbb <98395169+gglebbb@users.noreply.github.com>
2024-04-24 09:56:22 +0300

    Add Task 4-2

---
+++ zadanie4-2/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+
+namespace Task4_2
+
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
